b5d961aa19cfc242e1a82e7a0cb64dee38a4bbbf
sharadbhat-98 <sharathbhat563@gmail.com>
2021-03-26 13:55:49 +0530

    added responsive nav bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,8 +8,68 @@ import { LogoutCall, isAuthenticated } from './backendcalls/signupcall'
 import IconButton from '@material-ui/core/IconButton'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 
-const Header = () => {
-    const navButtons = () => {
+import { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
+import Drawer from '@material-ui/core/Drawer'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import { ListItemText } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}))
+
+function ElevationScroll(props) {
+    const { children, window } = props
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    })
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    })
+}
+
+ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+}
+
+const Header = (props) => {
+    const classes = useStyles()
+
+    const [view, setView] = useState({
+        mobileView: false,
+        drawerOpen: false,
+    })
+    const { mobileView, drawerOpen } = view
+
+    useEffect(() => {
+        const setResponsiveness = () => {
+            return window.innerWidth < 700
+                ? setView({ ...view, mobileView: true })
+                : setView({ ...view, mobileView: false })
+        }
+        setResponsiveness()
+        window.addEventListener('resize', () => setResponsiveness())
+    }, [])
+
+    const dispDesktop = () => {
         if (isAuthenticated()) {
             return (
                 <React.Fragment>
@@ -69,6 +129,69 @@ const Header = () => {
             )
         }
     }
+
+    const dispMobile = () => {
+        const handleDrawerOpen = () => {
+            setView({ ...view, drawerOpen: true })
+        }
+        const handleDrawerClose = () => {
+            setView({ ...view, drawerOpen: false })
+        }
+
+        return (
+            <React.Fragment>
+                <IconButton
+                    {...{
+                        edge: 'end',
+                        color: 'inherit',
+                        'aria-label': 'menu',
+                        'aria-haspopup': 'true',
+                        onClick: handleDrawerOpen,
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Drawer
+                    {...{
+                        anchor: 'top',
+                        open: drawerOpen,
+                        onClick: handleDrawerClose,
+                    }}
+                >
+                    <div>
+                        <ListItem button>
+                            <Link
+                                to="/profile"
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'inherit',
+                                }}
+                            >
+                                <ListItemText primary={'YOUR PROFILE'} />{' '}
+                                <Divider />
+                            </Link>
+                        </ListItem>
+                        <ListItem button>
+                            <Link
+                                to="/createblog"
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'inherit',
+                                }}
+                            >
+                                <ListItemText primary={'ADD A NEW BLOG'} />{' '}
+                                <Divider />
+                            </Link>
+                        </ListItem>
+                        <ListItem button onClick={LogoutCall}>
+                            <ListItemText primary={'LOGOUT'} />
+                        </ListItem>
+                    </div>
+                </Drawer>
+            </React.Fragment>
+        )
+    }
+
     return (
         <div style={{ paddingBottom: '65px' }}>
             <AppBar>
@@ -93,7 +216,9 @@ const Header = () => {
                             News{' '}
                         </Link>{' '}
                     </Typography>
-                    {navButtons()}
+                    <ElevationScroll {...props}>
+                        {mobileView ? dispMobile() : dispDesktop()}
+                    </ElevationScroll>
                 </Toolbar>
             </AppBar>
         </div>
--- src/protectedroutes.js
+++ src/protectedroutes.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import {Route,Redirect}from 'react-router-dom'
-import { isAuthenticated } from './components/backendcalls/signupcall';
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { isAuthenticated } from './components/backendcalls/signupcall'
 
-
-
-const ProtectedRoute = ({component:Component, ...rest}) => {
-    const test=isAuthenticated()
-    console.log(test)
-    return (  
-        <Route {...rest}
-            render={(props)=>{
-                if(test){
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const test = isAuthenticated()
+    return (
+        <Route
+            {...rest}
+            render={(props) => {
+                if (test) {
                     return <Component {...props} />
-                }else{
-                   return ( <Redirect to={{pathname:'/signup',state:{from:props.location}}}/>)
+                } else {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: '/signup',
+                                state: { from: props.location },
+                            }}
+                        />
+                    )
                 }
             }}
         />
-    );
+    )
 }
- 
-export default ProtectedRoute;
+
+export default ProtectedRoute
